11bf82e8f229c67e6b5b8232b5b54025015b700f
Cocosmos <mipam.guillot@crea-inseec.com>
2022-03-12 17:27:44 +0100

    new change

--- functions.php
+++ functions.php
@@ -2,5 +2,11 @@
 /**
  * MipamTheme.
  */
+//$theme_dir = get_template_directory();
+//require $theme_dir . '/inc/structure/header.php';
 
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+function theme_name_scripts() {
+   wp_enqueue_style( 'style-name', get_stylesheet_uri() . "style.css" );
+}
 ?>
