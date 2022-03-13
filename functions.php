6647aa898375235637c9b6bd68ac9c27b745eb42
Cocosmos <mipam.guillot@crea-inseec.com>
2022-03-13 23:48:15 +0100

    change theme

--- functions.php
+++ functions.php
@@ -2,11 +2,9 @@
 /**
  * MipamTheme.
  */
-//$theme_dir = get_template_directory();
-//require $theme_dir . '/inc/structure/header.php';
 
-add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
-function theme_name_scripts() {
-   wp_enqueue_style( 'style-name', get_stylesheet_uri() . "style.css" );
-}
+wp_enqueue_style( "total-child-css", get_stylesheet_directory_uri() . "../assets/css/style.min.css", array("total-parent-css") );
+
+
+
 ?>
